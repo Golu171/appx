df2f3a59e689aa28c47c91f6258887274a1ddec1
Golu171 <111356117+Golu171@users.noreply.github.com>
2025-12-29 18:53:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+API_ID = os.environ.get("API_ID", "16214694") #Replace With your api id
+API_HASH = os.environ.get("API_HASH", "7545825d90eb7adae543d59909c73121") #Replace With your api hash
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7708959953:AAFQUV1Fb9ErsnkI2ZKVQ6AKSkITSRa8l4s") #Replace With your bot token
